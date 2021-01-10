40e5fa46a0000922e95dedf5fef40c6b5f4fbd2b
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-11 00:15:17 +0100

    Added DB table definitions for reference

---
+++ db_tables/votes.sql
@@ -0,0 +1,18 @@
+CREATE TABLE votes (
+	id SERIAL PRIMARY KEY,
+	created_at TIMESTAMP WITH TIMEZONE DEFAULT CURRENT_TIMESTAMP,
+	user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+	option_id INTEGER REFERENCES options(id) ON DELETE CASCADE,
+	argument_id INTEGER REFERENCES arguments(id) ON DELETE CASCADE,
+	comment_id INTEGER REFERENCES comments(id) ON DELETE CASCADE,
+	v_type vote_type NOT NULL,
+	CHECK (
+		COALESCE((option_id)::BOOLEAN::INTEGER, 0)
+		+
+		COALESCE((argument_id)::BOOLEAN::INTEGER, 0)
+		+
+		COALESCE((comment_id)::BOOLEAN::INTEGER, 0)
+		= 1
+	),
+	UNIQUE(user_id, option_id, argument_id, comment_id)
+);
