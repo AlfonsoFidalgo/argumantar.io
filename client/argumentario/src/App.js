3d5cfff87f56cea42dd7ca6e7e892fcca8c67bf9
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-14 19:05:38 +0100

    Added client folder with create-react-app

---
+++ client/argumentario/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Argumentar.io</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
