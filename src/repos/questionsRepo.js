fd6723265da7f6cfdedbe0cc53129d098efb5e6a
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-20 19:52:49 +0100

    Changed the GET questions repo to get more info

--- src/repos/questionsRepo.js
+++ src/repos/questionsRepo.js
@@ -3,15 +3,54 @@ const pool = require('../pool');
 class QuestionRepo {
     static async getQuestions(){
         const query = `
-        SELECT
-            q.id,
-            q.created_at,
-            q.updated_at,
-            q.title,
-            q.body,
-            u.username
-        FROM questions q
-        JOIN users u ON u.id = q.user_id;
+        with 
+        questions_ as
+        (
+            SELECT
+                q.id as question_id,
+                q.created_at,
+                q.updated_at,
+                q.title as question_title,
+                q.body as question_body,
+                u.username
+            FROM questions q
+            JOIN users u ON u.id = q.user_id
+        ), 
+        question_engagement as
+        (
+            SELECT 
+                o.question_id,
+                COUNT(c.id) as question_engagement
+            FROM options o
+            FULL JOIN choices c ON c.option_id = o.id
+            GROUP BY 1
+        ),
+        options_ as 
+        (
+            SELECT 
+                o.id as option_id,
+                o.created_at,
+                o.updated_at,
+                o.question_id,
+                o.body as option_body,
+                COUNT(c.id) as support
+            FROM options o
+            
+            FULL JOIN choices c ON c.option_id = o.id
+            GROUP BY 1,2,3,4,5
+        )
+        SELECT 
+          q.question_id,
+          q.created_at,
+          q.question_title,
+          q.question_body,
+          q.username,
+          o.support as agree_support,
+          qe.question_engagement
+        FROM questions_ q
+        JOIN options_ o ON o.question_id = q.question_id
+        JOIN question_engagement qe ON qe.question_id = q.question_id
+        WHERE o.option_body = 'Agree';        
         `;
         const { rows } = await pool.query(query);
         return rows;
