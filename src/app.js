5698b01ac66d170bfe02a1957948e2315768e9a4
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-25 00:10:31 +0100

    forgot to save something

--- src/app.js
+++ src/app.js
@@ -32,7 +32,6 @@ module.exports = () => {
     app.use(commentsRouter);
     app.use(followersRouter);
     app.use((error, req, res, next) => {
-        // console.log(error);
         const status = error.statusCode || 500;
         const message = error.message;
         res.status(status).json(message)
