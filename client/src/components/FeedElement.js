659de712f6f3961826067f5975fb50d7867e8d3f
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-27 22:08:29 +0100

    Added react-router-dom plus some minor styling changes in FeedElement

--- client/src/components/FeedElement.js
+++ client/src/components/FeedElement.js
@@ -25,22 +25,22 @@ const FeedElement = (props) => {
     return (
             <Card className={classes.root}>
                 <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    {props.user} - {moment(props.date).format('Do MMM YY')}
-                </Typography>
-                <Typography variant="h6" component="h2">
-                    {props.title}
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    {props.arguments} people talking about this. <br/>
-                    {props.agreeRate *100}% agree, {props.disagreeRate *100}% disagree.
-                </Typography>
-                <Typography variant="body2" component="p">
-                    {props.body}
-                </Typography>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        {props.user} - {moment(props.date).format('Do MMM YY')}
+                    </Typography>
+                    <Typography variant="h6" component="h2">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" component="p">
+                        {props.body}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        {props.arguments} people talking about this. <br/>
+                        {props.agreeRate *100}% agree, {props.disagreeRate *100}% disagree.
+                    </Typography>
                 </CardContent>
                 <CardActions>
-                    <Button size="small">Read More</Button>
+                    <Button color="primary" variant="outlined" size="small">Read More</Button>
                 </CardActions>
             </Card>
     )
