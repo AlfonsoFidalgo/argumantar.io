82d70cf2d81d843b7b88276ab79e4cc080295704
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-27 23:21:28 +0100

    Added elevation scroll effect to the app bar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,23 @@
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
-//import ForumIcon from '@material-ui/icons/Forum';
+import { AppBar, Toolbar, Typography, useScrollTrigger } from '@material-ui/core';
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
+
+
+function ElevationScroll(props) {
+    const { children } = props;
+    // Note that you normally won't need to set the window ref as useScrollTrigger
+    // will default to window.
+    // This is only being set here because the demo is in an iframe.
+    const trigger = useScrollTrigger({
+      disableHysteresis: true,
+      threshold: 0
+    });
+  
+    return React.cloneElement(children, {
+      elevation: trigger ? 4 : 0,
+    });
+};
 
 const usedStyles = makeStyles(() => ({
     typographyStyles: { 
@@ -13,12 +28,14 @@ const usedStyles = makeStyles(() => ({
 const Header = () => {
     const classes = usedStyles();
     return (
-        <AppBar position='static'>
-            <Toolbar>
-                <Typography className={classes.typographyStyles}>Main Menu</Typography>
-                <AccountCircleIcon/>
-            </Toolbar>
-        </AppBar>
+        <ElevationScroll>
+            <AppBar position='fixed'>
+                <Toolbar>
+                    <Typography className={classes.typographyStyles}>wescuss</Typography>
+                    <AccountCircleIcon/>
+                </Toolbar>
+            </AppBar>
+        </ElevationScroll>
     )
 };
 
