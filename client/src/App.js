fa99abc1d74b40fb5ea2b193a053975a18fa0ea7
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-27 23:51:36 +0100

    Added Theme file and applied it to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
 import Header from './components/Header';
 import QuestionsFeed from './components/QuestionsFeed';
+import { ThemeProvider } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
+import theme from './Theme';
 
 function App() {
   return (
-    <Grid container direction='column'>
-      <Grid item>
-        <Header />
-      </Grid>
-
-      <Grid item container>
-        <Grid item xs={false} sm={2}/>
-        <Grid item xs={12} sm={8}>
-          <QuestionsFeed />
+    <ThemeProvider theme={theme}>
+      <Grid container direction='column'>
+        <Grid item>
+          <Header />
         </Grid>
-        <Grid item xs={false} sm={2}/>
+
+        <Grid item container>
+          <Grid item xs={false} sm={2}/>
+          <Grid item xs={12} sm={8}>
+            <QuestionsFeed />
+          </Grid>
+          <Grid item xs={false} sm={2}/>
+        </Grid>
       </Grid>
-    </Grid>
+    </ThemeProvider>
   );
 }
 
