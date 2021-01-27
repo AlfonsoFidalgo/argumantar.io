fb4c28ae9ab3869ad0ac0c5245efe3ebb9ec6c3d
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-27 09:55:16 +0100

    Fixed a little typo in makeStyles()

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,25 +1,22 @@
 import { AppBar, Toolbar, Typography } from '@material-ui/core';
-import ForumIcon from '@material-ui/icons/Forum';
+//import ForumIcon from '@material-ui/icons/Forum';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import { makeStyles } from '@material-ui/styles';
 import React from 'react';
 
-const usedStyles = makeStyles(() => {
-    return({
-        typographyStyles: { 
-            flex: 1
-        }
-    }
-    )
-});
+const usedStyles = makeStyles(() => ({
+    typographyStyles: { 
+        flex: 1
+    } 
+}));
 
 const Header = () => {
-    const classes = usedStyles;
+    const classes = usedStyles();
     return (
         <AppBar position='static'>
             <Toolbar>
-                
                 <Typography className={classes.typographyStyles}>Main Menu</Typography>
-                <ForumIcon />
+                <AccountCircleIcon/>
             </Toolbar>
         </AppBar>
     )
