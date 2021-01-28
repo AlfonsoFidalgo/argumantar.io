4d6e37a51309379e4df27854b441e0623dce1fd9
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-28 22:57:33 +0100

    Finished the header component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,6 @@
-import { AppBar, Toolbar, Typography, useScrollTrigger, Tabs, Tab } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, useScrollTrigger, Tabs, Tab, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 
@@ -31,22 +31,41 @@ const usedStyles = makeStyles(theme => ({
     },
     styledIndicator: {
         backgroundColor: '#0c1613'
+    },
+    logoContainer: {
+        padding: 0,
+        '&:hover': {
+            backgroundColor: 'transparent'
+        }
     }
 }));
 
 const Header = () => {
     const classes = usedStyles();
     const [value, setValue] = useState(0);
+
     const handleChange = (e, value) => {
         setValue(value);
     };
+
+    useEffect(() => {
+        if (window.location.pathname === '/' && value !== 0){
+            setValue(0);
+        } else if (window.location.pathname === '/newpost' && value !== 1){
+            setValue(1);
+        } else if (window.location.pathname === '/login' && value !== 2){
+            setValue(2);
+        }
+    }, [value]);
 
     return (
         <React.Fragment>
             <ElevationScroll>
                 <AppBar position='fixed'>
                     <Toolbar>
-                        <Typography className={classes.typographyStyles} variant='h5'>wescuss</Typography>
+                        <Button component={Link} to='/' className={classes.logoContainer} onClick={() => setValue(0)} disableRipple>
+                            <Typography className={classes.typographyStyles} variant='h5'>wescuss</Typography>
+                        </Button>
                         <Tabs value={value} onChange={handleChange} className={classes.tabContainer} classes={{indicator: classes.styledIndicator}} >
                             <Tab component={Link} to='/' label="Home" />
                             <Tab component={Link} to='/newpost' label="New Post" />
