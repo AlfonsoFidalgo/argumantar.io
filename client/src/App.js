d0dce558f0c777957c8364400a12c62c53b270c3
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-30 14:04:29 +0100

    Added Login component

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@material-ui/styles';
 import { Grid } from '@material-ui/core';
 import Header from './components/Header';
 import QuestionsFeed from './components/QuestionsFeed';
+import Login from './components/Login';
 import theme from './Theme';
 
 function App() {
@@ -16,14 +17,14 @@ function App() {
             </Grid>
           <Grid item container>
             <Grid item xs={false} sm={2}/>
-              <Grid item xs={12} sm={8}>
-                <Switch>
-                  <Route exact path='/' component={QuestionsFeed}/>
-                  <Route exact path='/newpost' component={() => <div>NEW POST GOES HERE</div>}/>
-                  <Route exact path='/signup' component={() => <div>SIGN UP PAGE GOES HERE</div>}/>
-                  <Route exact path='/login' component={() => <div>LOGIN PAGE GOES HERE</div>}/>
-                </Switch>
-              </Grid>
+            <Grid item xs={12} sm={8}>
+              <Switch>
+                <Route exact path='/' component={QuestionsFeed}/>
+                <Route exact path='/newpost' component={() => <div>NEW POST GOES HERE</div>}/>
+                <Route exact path='/signup' component={() => <div>SIGN UP PAGE GOES HERE</div>}/>
+                <Route exact path='/login' component={Login}/>
+              </Switch>
+            </Grid>
             <Grid item xs={false} sm={2}/>
           </Grid>
         </Grid>
---
+++ client/src/components/Login.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Grid, TextField, Typography, Button, Avatar, Container } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+
+const Login = (props) => {
+    const classes = useStyles();
+
+    return (
+        (
+        <Container component="main" maxWidth="xs">
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                    <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Welcome Back!
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        autoFocus
+                    />
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                    />
+                    <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    >
+                    Sign In
+                    </Button>
+                </form>
+            </div>
+        </Container>
+        )
+    )
+};
+
+export default Login;
