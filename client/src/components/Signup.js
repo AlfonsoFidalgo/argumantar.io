061edf5859946812cf51c63341c9bb0d8766a99e
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-01-31 11:56:36 +0100

    Added signup flow

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import {Avatar, Button, TextField, Grid, makeStyles, Container, Typography} from '@material-ui/core';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -26,12 +27,43 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-export default function Signup() {
+export default function Signup(props) {  
+  const [emailState, setEmailState] = useState('');
+  const [passwordState, setPasswordState] = useState('');
+  const [usernameState, setUsernameState] = useState('');
+  const [displayNameState, setDisplayNameState] = useState('');
+  const [confirmedPasswordState, setConfirmedPasswordState] = useState('');
   const classes = useStyles();
+  
+  const usernameHandler = (e) => setUsernameState(e.target.value);
+  const displaynameHandler = (e) => setDisplayNameState(e.target.value);
+  const emailHandler = (e) => setEmailState(e.target.value);
+  const passwordHandler = (e) => setPasswordState(e.target.value);
+  const confirmedPasswordHandler = (e) => setConfirmedPasswordState(e.target.value);
+
+  const signupHandler = (e) => {
+      e.preventDefault();
+      if (passwordState === confirmedPasswordState){
+          if (passwordState.length > 7){
+              const data = {
+                  email: emailState,
+                  username: usernameState,
+                  displayName: displayNameState,
+                  password: passwordState
+              };
+              axios.post('http://localhost:3001/user/signup', data)
+              .then(response => console.log(response.data))
+              .catch(err => console.log(err.response.data))
+          } else {
+              console.log('Please, provide a longer password (min. 8 characters)');
+          }
+      } else {
+          console.log('Password and confirmed password fields don\'t match');
+      }
+  };
 
   return (
     <Container component="main" maxWidth="xs">
-      {/* <CssBaseline /> */}
       <div className={classes.paper}>
         <Avatar className={classes.avatar}>
           <LockOutlinedIcon />
@@ -39,7 +71,7 @@ export default function Signup() {
         <Typography component="h1" variant="h5">
           Sign up
         </Typography>
-        <form className={classes.form} >
+        <form className={classes.form} onSubmit={signupHandler} >
           <Grid container spacing={2}>
             <Grid item xs={12} sm={6}>
               <TextField
@@ -50,6 +82,8 @@ export default function Signup() {
                 id="userName"
                 label="Username"
                 autoFocus
+                value={usernameState}
+                onChange={usernameHandler}
               />
             </Grid>
             <Grid item xs={12} sm={6}>
@@ -59,6 +93,8 @@ export default function Signup() {
                 id="displayName"
                 label="Display Name"
                 name="displayName"
+                value={displayNameState}
+                onChange={displaynameHandler}
               />
             </Grid>
             <Grid item xs={12}>
@@ -70,6 +106,8 @@ export default function Signup() {
                 label="Email Address"
                 name="email"
                 autoComplete="email"
+                value={emailState}
+                onChange={emailHandler}
               />
             </Grid>
             <Grid item xs={12}>
@@ -82,6 +120,8 @@ export default function Signup() {
                 type="password"
                 id="password"
                 autoComplete="current-password"
+                value={passwordState}
+                onChange={passwordHandler}
               />
             </Grid>
             <Grid item xs={12}>
@@ -94,6 +134,8 @@ export default function Signup() {
                 type="password"
                 id="confirmedPassword"
                 autoComplete="current-password"
+                value={confirmedPasswordState}
+                onChange={confirmedPasswordHandler}
               />
             </Grid>
           </Grid>
