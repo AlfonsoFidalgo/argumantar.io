f40ae0ef496af6229cedd69b6e68288c2fdd5f9a
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-04 00:27:15 +0100

    Added a Divider component in the argument

--- client/src/components/ArgumentMeta.js
+++ client/src/components/ArgumentMeta.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import { Grid, Typography, ButtonGroup, IconButton } from '@material-ui/core';
+import { Grid, Typography, ButtonGroup, IconButton, Divider } from '@material-ui/core';
 import { ThumbUp, ThumbDown } from '@material-ui/icons';
 
 const useStyles = makeStyles(theme => ({
@@ -14,17 +14,20 @@ const useStyles = makeStyles(theme => ({
 const ArgumentMeta = (props) => {
     const classes = useStyles();
     return(
+        <React.Fragment>
         <Grid container>
             <Grid item xs={6}>
                 <Typography color="textSecondary">{props.username} - {props.date}</Typography>
             </Grid>
             <Grid item xs={6} className={classes.choiceButtons}>
                 <ButtonGroup size="small" >
-                    <IconButton><ThumbUp/></IconButton>
-                    <IconButton><ThumbDown/></IconButton>
+                    <IconButton size='small'><ThumbUp/></IconButton>
+                    <IconButton size='small'><ThumbDown/></IconButton>
                 </ButtonGroup>
             </Grid>
         </Grid>
+        <Divider />
+        </React.Fragment>
     )
 };
 
