ba28e5a0f2793c78347d4faf5d8542fd08c81f3f
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-04 23:17:22 +0100

    Fetch arguments data from Arguments component and store it in state

--- client/src/components/Arguments.js
+++ client/src/components/Arguments.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { List, ListItem, ListItemText, Typography,
     Accordion, AccordionSummary, AccordionDetails } from '@material-ui/core';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import axios from 'axios';
 import ArgumentMeta from './ArgumentMeta';
 
 
@@ -34,6 +35,16 @@ const useStyles = makeStyles(theme => ({
 
 
 const Arguments = (props) => {
+    const [argumentsState, setArgumentsState] = useState();
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await axios.get('http://localhost:3001/question/15/arguments/get');
+            setArgumentsState(response.data);
+        }
+        fetchData();
+        console.log(argumentsState);
+    }, []);
 
     const classes = useStyles();
     return (
@@ -45,10 +56,6 @@ const Arguments = (props) => {
             <AccordionDetails >
                 
                 <List>
-                    <ListItem>
-                        <ListItemText primary='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-                         secondary={<ArgumentMeta username='fonsete' date='2 Feb 2021'/>} />
-                    </ListItem>
                     <ListItem>
                         <ListItemText primary='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
                          secondary={<ArgumentMeta username='fonsete' date='2 Feb 2021'/>} />
