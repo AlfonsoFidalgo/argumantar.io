a34c71114bdd7c61eb1eee135ab313be1e26c285
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-11 00:24:09 +0100

    Installed redux and added actions and reducer

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
