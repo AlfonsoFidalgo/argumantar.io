06cf448df874fe6165c3468f024b21bb0643155e
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-11 22:54:54 +0100

    Added some reducer logic to add and remove agrees/disagrees

--- client/src/store/actions.js
+++ client/src/store/actions.js
@@ -1,3 +1,4 @@
 export const AGREE = 'AGREE';
 export const DISAGREE = 'DISAGREE';
-
+export const REMOVE_AGREE = 'REMOVE_AGREE';
+export const REMOVE_DISAGREE = 'REMOVE_DISAGREE';
--- client/src/store/reducer.js
+++ client/src/store/reducer.js
@@ -6,7 +6,34 @@ const initialState = {
 };
 
 const reducer = (state = initialState, action) => {
-
+    const agreedQuestions = [...initialState.agreedQuestions];
+    const disagreedQuestions = [...initialState.disagreedQuestions];
+    switch (action.type){
+        case actionTypes.AGREE:
+            return {
+                ...state,
+                agreedQuestions: agreedQuestions.push(action.payload.questionId)
+            }
+        case actionTypes.REMOVE_AGREE:
+            agreedQuestions.splice(agreedQuestions.indexOf(action.payload.questionId), 1);
+            return {
+                ...state,
+                agreedQuestions: agreedQuestions
+            }
+        case actionTypes.DISAGREE:
+            return {
+                ...state,
+                disagreedQuestions: disagreedQuestions.push(action.payload.questionId)
+            }
+        case actionTypes.REMOVE_DISAGREE:
+            disagreedQuestions.splice(disagreedQuestions.indexOf(action.payload.questionId), 1)
+            return {
+                ...state,
+                disagreedQuestions: disagreedQuestions
+            }
+        default:
+            return state
+    }
 };
 
 export default reducer;
