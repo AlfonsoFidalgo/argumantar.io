bdf5fa731ee808b2d00986abd90c41264db53311
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-12 23:01:33 +0100

    Added a logging middleware to see info from actions and store

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
 
+//middleware
+const logger = (store) => {
+    return (next) => {
+        return (action) => {
+            console.log('@from middleware@ dispatching:', action);
+            const result = next(action);
+            console.log('@from middleware@ next state:', store.getState());
+            return result;
+        }
+    }
+};
+
+const store = createStore(reducer, applyMiddleware(logger));
 
 ReactDOM.render(
     <Provider store={store}>
