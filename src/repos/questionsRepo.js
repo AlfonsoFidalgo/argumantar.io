50f50bac80bec40b8fbb2a1ee2077a7d7d83e342
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-14 21:56:40 +0100

    Improved the feed query to get better structured and complete data

--- src/repos/questionsRepo.js
+++ src/repos/questionsRepo.js
@@ -4,65 +4,66 @@ class QuestionRepo {
     static async getQuestions(){
         const query = `
         WITH 
-        questions_ as
-        (
-            SELECT
-                q.id as question_id,
-                q.created_at,
-                q.updated_at,
-                q.title as question_title,
-                q.body as question_body,
-                u.username
-            FROM questions q
-            JOIN users u ON u.id = q.user_id
-        ), 
-        question_engagement as
-        (
-            SELECT 
+            questions_ as
+            (
+                SELECT
+                    q.id as question_id,
+                    q.created_at,
+                    q.updated_at,
+                    q.title as question_title,
+                    q.body as question_body,
+                    u.username,
+                    u.display_name
+                FROM questions q
+                JOIN users u ON u.id = q.user_id
+            ),
+            arguments_ as
+            (
+                SELECT 
                 o.question_id,
-                COUNT(c.id) as question_engagement
-            FROM options o
-            FULL JOIN choices c ON c.option_id = o.id
-            GROUP BY 1
-        ),
-        arguments_ as
-        (
-            SELECT 
-            o.question_id,
-            count(a.id) as num_arguments
-            FROM options o
-            JOIN arguments a ON a.option_id = o.id
-            GROUP BY 1
-        ),
-        options_ as 
-        (
-            SELECT 
-                o.id as option_id,
-                o.created_at,
-                o.updated_at,
-                o.question_id,
-                o.body as option_body,
-                COUNT(c.id) as support
-            FROM options o
-            
-            FULL JOIN choices c ON c.option_id = o.id
-            GROUP BY 1,2,3,4,5
-        )
+                count(a.id) as num_arguments
+                FROM options o
+                JOIN arguments a ON a.option_id = o.id
+                GROUP BY 1
+            ),
+            agree_options as 
+            (
+                select 
+                     o.question_id,
+                     o.id as agree_option_id,
+                     count(c.id) as agree_votes
+                from options o
+                full join choices c ON c.option_id = o.id
+                where body = 'Agree'
+                group by 1,2
+            ),
+            disagree_options as
+            (
+                select 
+                     o.question_id,
+                     o.id as disagree_option_id,
+                     count(c.id) as disagree_votes
+                from options o
+                full join choices c ON c.option_id = o.id
+                where body = 'Disagree'
+                group by 1,2
+            )
         SELECT 
         q.question_id,
         q.created_at,
         q.question_title,
         q.question_body,
         q.username,
-        o.support as agree_support,
-        qe.question_engagement,
-        COALESCE(a.num_arguments, 0) as num_arguments
+        q.display_name,
+        COALESCE(a.num_arguments, 0) as num_arguments,
+        aop.agree_option_id,
+        aop.agree_votes,
+        dop.disagree_option_id,
+        dop.disagree_votes
         FROM questions_ q
-        JOIN options_ o ON o.question_id = q.question_id
-        JOIN question_engagement qe ON qe.question_id = q.question_id
-        FULL JOIN arguments_ a ON a.question_id = q.question_id
-        WHERE o.option_body = 'Agree'
-        ORDER BY q.created_at DESC;
+        JOIN agree_options aop ON aop.question_id = q.question_id
+        JOIN disagree_options dop ON dop.question_id = q.question_id
+        FULL JOIN arguments_ a ON a.question_id = q.question_id; 
         `;
         const { rows } = await pool.query(query);
         return rows;
@@ -97,66 +98,69 @@ class QuestionRepo {
 
     static async getQuestionById(id){
         const query = `
+        --improved feed with additional columns and more structured
         WITH 
-        questions_ as
-        (
-            SELECT
-                q.id as question_id,
-                q.created_at,
-                q.updated_at,
-                q.title as question_title,
-                q.body as question_body,
-                u.username
-            FROM questions q
-            JOIN users u ON u.id = q.user_id
-        ), 
-        question_engagement as
-        (
-            SELECT 
+            questions_ as
+            (
+                SELECT
+                    q.id as question_id,
+                    q.created_at,
+                    q.updated_at,
+                    q.title as question_title,
+                    q.body as question_body,
+                    u.username,
+                    u.display_name
+                FROM questions q
+                JOIN users u ON u.id = q.user_id
+            ),
+            arguments_ as
+            (
+                SELECT 
                 o.question_id,
-                COUNT(c.id) as question_engagement
-            FROM options o
-            FULL JOIN choices c ON c.option_id = o.id
-            GROUP BY 1
-        ),
-        arguments_ as
-        (
-            SELECT 
-            o.question_id,
-            count(a.id) as num_arguments
-            FROM options o
-            JOIN arguments a ON a.option_id = o.id
-            GROUP BY 1
-        ),
-        options_ as 
-        (
-            SELECT 
-                o.id as option_id,
-                o.created_at,
-                o.updated_at,
-                o.question_id,
-                o.body as option_body,
-                COUNT(c.id) as support
-            FROM options o
-            
-            FULL JOIN choices c ON c.option_id = o.id
-            GROUP BY 1,2,3,4,5
-        )
+                count(a.id) as num_arguments
+                FROM options o
+                JOIN arguments a ON a.option_id = o.id
+                GROUP BY 1
+            ),
+            agree_options as 
+            (
+                select 
+                     o.question_id,
+                     o.id as agree_option_id,
+                     count(c.id) as agree_votes
+                from options o
+                full join choices c ON c.option_id = o.id
+                where body = 'Agree'
+                group by 1,2
+            ),
+            disagree_options as
+            (
+                select 
+                     o.question_id,
+                     o.id as disagree_option_id,
+                     count(c.id) as disagree_votes
+                from options o
+                full join choices c ON c.option_id = o.id
+                where body = 'Disagree'
+                group by 1,2
+            )
         SELECT 
         q.question_id,
         q.created_at,
         q.question_title,
         q.question_body,
         q.username,
-        o.support as agree_support,
-        qe.question_engagement,
-        COALESCE(a.num_arguments, 0) as num_arguments
+        q.display_name,
+        COALESCE(a.num_arguments, 0) as num_arguments,
+        aop.agree_option_id,
+        aop.agree_votes,
+        dop.disagree_option_id,
+        dop.disagree_votes
         FROM questions_ q
-        JOIN options_ o ON o.question_id = q.question_id
-        JOIN question_engagement qe ON qe.question_id = q.question_id
-        FULL JOIN arguments_ a ON a.question_id = q.question_id
-        WHERE o.option_body = 'Agree'
-        AND q.question_id = $1;
+        JOIN agree_options aop ON aop.question_id = q.question_id
+        JOIN disagree_options dop ON dop.question_id = q.question_id
+        FULL JOIN arguments_ a ON a.question_id = q.question_id;
+        WHERE question_id = $1
         `;
         const { rows } = await pool.query(query, [id]);
         return rows;
