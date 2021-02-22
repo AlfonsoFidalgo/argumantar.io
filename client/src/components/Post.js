16dbc5748a009aaad0124789e063119963eb64b1
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-02-23 00:14:41 +0100

    Post component displays data from Redux store

--- client/src/components/Post.js
+++ client/src/components/Post.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
 import { Card, CardActions, CardContent, Button, ButtonGroup, Typography, Grid, TextField } from '@material-ui/core';
 import moment from 'moment';
 import Arguments from './Arguments';
+import Spinner from './Spinner';
+import * as actions from '../store/actions';
 
 
 const useStyles = makeStyles(theme => ({
@@ -32,29 +35,32 @@ const useStyles = makeStyles(theme => ({
 }));
 
 const Post = (props) => {
-    const [userState, setUserState] = useState();
-    const [dateState, setDateState] = useState();
-    const [titleState, setTitleState] = useState();
-    const [bodyState, setBodyState] = useState();
-    const [numArgsState, setNumArgsState] = useState();
-    const [agreeRateState, setAgreeRateState] = useState();
-    const [disagreeRateState, setDisagreeRateState] = useState(); 
+    // const [userState, setUserState] = useState();
+    // const [dateState, setDateState] = useState();
+    // const [titleState, setTitleState] = useState();
+    // const [bodyState, setBodyState] = useState();
+    // const [numArgsState, setNumArgsState] = useState();
+    // const [agreeRateState, setAgreeRateState] = useState();
+    // const [disagreeRateState, setDisagreeRateState] = useState(); 
     const [argumentState, setArgumentState] = useState();
-    const [agreeOptionIdState, setAgreeOptionIdState] = useState();
-    const [disagreeOptionIdState, setDisagreeOptionIdState] = useState();
+    // const [agreeOptionIdState, setAgreeOptionIdState] = useState();
+    // const [disagreeOptionIdState, setDisagreeOptionIdState] = useState();
     const [choiceButtonsState, setChoiceButtonsState] = useState({agree: 'outlined', disagree: 'outlined'});
 
     useEffect(() => {
-        setUserState(props.location.state.username);
-        setDateState(props.location.state.createdAt);
-        setTitleState(props.location.state.questionTitle);
-        setBodyState(props.location.state.questionBody);
-        setNumArgsState(props.location.state.numArguments);
-        setAgreeRateState(props.location.state.agreeRate);
-        setDisagreeRateState(props.location.state.disagreeRate);
-        setAgreeOptionIdState(props.location.state.agreeOptionId);
-        setDisagreeOptionIdState(props.location.state.disagreeOptionId);
-    }, [props]);
+        props.onQuestionLoad(props.match.params.id);
+        // setUserState(props.location.state.username);
+        // setDateState(props.location.state.createdAt);
+        // setTitleState(props.location.state.questionTitle);
+        // setBodyState(props.location.state.questionBody);
+        // setNumArgsState(props.location.state.numArguments);
+        // setAgreeRateState(props.location.state.agreeRate);
+        // setDisagreeRateState(props.location.state.disagreeRate);
+        // setAgreeOptionIdState(props.location.state.agreeOptionId);
+        // setDisagreeOptionIdState(props.location.state.disagreeOptionId);
+    }, []);
+
+    const classes = useStyles();
 
     const handleArgument = (e) => {
         setArgumentState(e.target.value);
@@ -86,59 +92,79 @@ const Post = (props) => {
         };
     };
 
-    const classes = useStyles();
-    return (
-        <Card className={classes.root}>
-            <CardContent>
-                <Typography className={classes.title} color="textSecondary" gutterBottom>
-                    {userState} - {moment(dateState).format('Do MMM YY')}
-                </Typography>
-                <Typography variant="h6" component="h2">
-                    {titleState}
-                </Typography>
-                <Typography variant="body1" component="p">
-                    {bodyState}
-                </Typography>
-                <Typography className={classes.pos} color="textSecondary">
-                    {numArgsState} people talking about this. <br/>
-                    {agreeRateState * 100}% agree, {disagreeRateState * 100}% disagree.
-                </Typography>
-            </CardContent>
-            <CardActions>
-                <Grid container>
-                    <Grid item xs={6}>
-                        <Button 
-                        component={Link} 
-                        to='/'
-                        color="primary" variant="outlined" size="small">Back</Button>
-                    </Grid>
-                    <Grid item xs={6} className={classes.choiceButtons}>
-                        <ButtonGroup color="primary" size="small" fullWidth aria-label="outlined secondary button group">
-                            <Button variant={choiceButtonsState.agree} onClick={(e) => handleChoice(e, 'agree')}>Agree</Button>
-                            <Button variant={choiceButtonsState.disagree} onClick={(e) => handleChoice(e, 'disagree')}>Disagree</Button>
-                        </ButtonGroup>
-                    </Grid>
-                    <Grid item xs={12} className={classes.argumentBox}>
-                        <TextField
-                        id="standard-multiline-static"
-                        label="Share your arguments"
-                        multiline
-                        fullWidth
-                        variant="outlined"
-                        rows={6}
-                        onChange={handleArgument}
-                        value={argumentState}
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button color="primary" variant="contained" fullWidth>Send</Button>
-                    </Grid>
-                </Grid>
-            </CardActions>
+    let activeQuestion = <Spinner />;
+    console.log(props)
 
-            <Arguments questionId={props.match.params.id}/>
-        </Card>
-    );
+    if (props.activeQuestion){
+        const agreeVotes = props.activeQuestion[0].agree_votes;
+        const disagreeVotes = props.activeQuestion[0].disagree_votes;
+        activeQuestion = (
+            <Card className={classes.root}>
+                <CardContent>
+                    <Typography className={classes.title} color="textSecondary" gutterBottom>
+                        {props.activeQuestion[0].username} - {moment(props.activeQuestion[0].created_at).format('Do MMM YY')}
+                    </Typography>
+                    <Typography variant="h6" component="h2">
+                        {props.activeQuestion[0].question_title}
+                    </Typography>
+                    <Typography variant="body1" component="p">
+                        {props.activeQuestion[0].question_body}
+                    </Typography>
+                    <Typography className={classes.pos} color="textSecondary">
+                        {props.activeQuestion[0].num_arguments} people talking about this. <br/>
+                        {(agreeVotes / (agreeVotes + disagreeVotes)) * 100}% agree, {(disagreeVotes / (agreeVotes + disagreeVotes)) * 100}% disagree.
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <Grid container>
+                        <Grid item xs={6}>
+                            <Button 
+                            component={Link} 
+                            to='/'
+                            color="primary" variant="outlined" size="small">Back</Button>
+                        </Grid>
+                        <Grid item xs={6} className={classes.choiceButtons}>
+                            <ButtonGroup color="primary" size="small" fullWidth aria-label="outlined secondary button group">
+                                <Button variant={choiceButtonsState.agree} onClick={(e) => handleChoice(e, 'agree')}>Agree</Button>
+                                <Button variant={choiceButtonsState.disagree} onClick={(e) => handleChoice(e, 'disagree')}>Disagree</Button>
+                            </ButtonGroup>
+                        </Grid>
+                        <Grid item xs={12} className={classes.argumentBox}>
+                            <TextField
+                            id="standard-multiline-static"
+                            label="Share your arguments"
+                            multiline
+                            fullWidth
+                            variant="outlined"
+                            rows={6}
+                            onChange={handleArgument}
+                            value={argumentState}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button color="primary" variant="contained" fullWidth>Send</Button>
+                        </Grid>
+                    </Grid>
+                </CardActions>
+
+                <Arguments questionId={props.match.params.id}/>
+            </Card>
+        );
+    };
+    
+    return (activeQuestion)
 };
 
-export default Post;
+const mapStateToProps = state => {
+    return {
+        activeQuestion: state.questions.activeQuestion
+    };
+};
+
+const mapDispatchToProps = dispatch => {
+    return {
+        onQuestionLoad: (questionId) => dispatch(actions.fetchQuestion(questionId))
+    };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
