6235354b0a419790e0714400fece8d6e8e7b867a
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-03-07 21:06:07 +0100

    Added a logout component that redirects to the feed

--- client/src/components/Logout.js
+++ client/src/components/Logout.js
@@ -1,8 +1,23 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
+import { Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../store/actions';
 
 const Logout = (props) => {
 
+    useEffect(() => {
+        props.onLogout();
+    });
+
+    return (
+        <Redirect to='/' />
+    );
 };
 
+const mapDispatchToProps = dispatch => {
+    return {
+        onLogout: () => dispatch(actions.logout())
+    };
+}
 
-export default Logout;
+export default connect(null, mapDispatchToProps)(Logout);
