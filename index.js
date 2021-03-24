a605ba8e922ec63561485a1782f62f9a768c16d2
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-03-24 22:39:01 +0100

    ssl option

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ pool.connect({
     port: process.env.DATABASE_PORT || 5432,
     database: process.env.DATABASE || 'argumentario',
     user: process.env.DATABASE_USER || 'alfonso.fidalgo',
-    password: process.env.DATABASE_PASSWORD || ''
+    password: process.env.DATABASE_PASSWORD || '',
+    ssl: true
 })
 .then(() => {
     app().listen(PORT, () => console.log(`app running on ${PORT}`));
