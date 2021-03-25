615425bd3cf714352595cfdf3dede8f3e50ae7f9
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-03-25 22:34:55 +0100

    Updated all the urls from the actions file

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
     app.use(
-      "/questions",
+      "/api",
       createProxyMiddleware({
         target: "http://localhost:3001",
       })
