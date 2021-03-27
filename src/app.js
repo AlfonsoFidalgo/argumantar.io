a6ed912d9fac247dd624c99d14951bdba0352bf0
Alfonso Fidalgo <fidalgo.alfonso@gmail.com>
2021-03-27 23:11:28 +0100

    Change path resolve

--- src/app.js
+++ src/app.js
@@ -44,8 +44,7 @@ module.exports = () => {
         //express serves index.html if it doesn't recognise the route
         const path = require('path');
         app.get('*', (req, res) => {
-            console.log('ok');
-            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+            res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
         });
     };
 
